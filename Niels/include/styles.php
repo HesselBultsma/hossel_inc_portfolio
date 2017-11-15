c737294dbaf3d82bd09e9f4aabb05a02f405b54f
johnmestebeld <john@dehaanmedia.net>
2017-11-15 10:25:14 +0100

    added personal workfolders
    # Conflicts:
    #	Hessel/www/src/css/animate.css
    #	Hessel/www/src/css/bootstrap.min.css
    #	Hessel/www/src/css/one-page-wonder.css
    #	Hessel/www/src/index.html
    #	Hessel/www/src/js/bootstrap.bundle.js
    #	Hessel/www/src/js/bootstrap.min.js
    #	Hessel/www/src/js/jquery-3.2.1.min.js
    #	css/animate.css
    #	css/bootstrap.min.css
    #	css/one-page-wonder.css
    #	js/bootstrap.bundle.js
    #	js/bootstrap.min.js
    #	js/jquery-3.2.1.min.js
    #	old plain html/www/src/index.html
    #	www/src/css/animate.css
    #	www/src/css/bootstrap.min.css
    #	www/src/css/one-page-wonder.css
    #	www/src/index.html
    #	www/src/js/bootstrap.bundle.js
    #	www/src/js/bootstrap.min.js
    #	www/src/js/jquery-3.2.1.min.js

---
+++ Niels/include/styles.php
@@ -0,0 +1,25 @@
+<!-- saved from url=(0067)https://blackrockdigital.github.io/startbootstrap-one-page-wonder/# -->
+<html lang="en" wtx-context="B8B5AD37-D27A-4230-885C-BCA3B19419CE"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>One Page Wonder - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+     <link href="./css/animate.css" rel="stylesheet">
+     
+     
+     <script src="./js/custom.js"></script>
+      
+
+
+   
+    <!-- Custom styles for this template -->
+    <link href="./css/one-page-wonder.css" rel="stylesheet">
+    <link href="./css/styles.css" rel="stylesheet">
+
+</head>
---
+++ Sybren/include/footer.php
@@ -0,0 +1,9 @@
+    <!-- Footer -->
+    <section style="background: #3d3d3d;">
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright © Hossel Inc. 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+    </section>
