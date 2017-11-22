ae482be566e46150dadf9c487bde2283eef3d96a
johnmestebeld <john@dehaanmedia.net>
2017-11-22 15:33:46 +0100

    demo addprojects

---
+++ John/public_html/addproject.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<?php include 'include/styles.php';?>
+<?php //include 'include/menu.php';?>
+
+<body cz-shortcut-listen="true">
+
+
+    <?php include 'include/menu.php';?>
+
+
+
+
+    <section class="pb-5">
+        <div class="container pt-5 pb-4">
+
+            <div class="row ">
+                <div class="col-12 text-center">
+                    <h1>Add project</h1>
+                </div>
+
+
+
+            </div>
+        </div>
+
+
+        
+
+
+        <div class="container  pb-4">
+            <div class="row">
+
+
+
+
+
+                <script>
+                    for (i = 0; i < 10; i++) {
+                        document.write("<div class='col-lg-6 portfolio-item'><div class='card h-100 wow slideInUp'><a href='#'><img class='card-img-top' src='http://placehold.it/700x400' alt=''></a><div class='card-body'><h4 class='card-title'><a href='#'>Project</a></h4><p>description</p></div></div></div>");
+     
+                                   
+                        
+                       
+                    }
+                    
+                    
+           
+
+                </script>
+
+
+
+
+            </div>
+        </div>
+    </section>
+
+
+
+    <?php include 'include/footer.php';?>
+
+    <?php include 'include/scripts.php';?>
